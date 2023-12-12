4b116b35d8e511ac817e1e16b1aaff49b48156f1
joeygarberick <112030126+joeygarberick@users.noreply.github.com>
2023-12-11 20:59:53 -0500

    added tab 2

--- gameapp.R
+++ gameapp.R
@@ -1,10 +1,17 @@
 library(shiny)
 library(ggplot2)
+library(dplyr)
+library(tidyr)
 
 game_data <- read.csv(file = "BGG_Data_Set.csv")
 
+domain_data <- game_data %>% separate_rows(Domains, sep = "\\, ")
+
+mechanic_data <- game_data %>% separate_rows(Mechanics, sep = "\\, ")
+
 
 ui <- fluidPage(tabsetPanel(
+  tabPanel("Table", dataTableOutput(outputId = "data")),
   tabPanel("Histogram",
            sidebarLayout(
              sidebarPanel(
@@ -23,13 +30,40 @@ ui <- fluidPage(tabsetPanel(
                    "Owned.Users"
                    )
                  ),
-               sliderInput(inputId = "b", label = "Bins", min = 1, max = 40, value = 5)
+               sliderInput(inputId = "b", label = "Bins", min = 1, max = 40, value = 5),
+               numericInput(inputId = "c", label = 'X min', value = 0),
+               numericInput(inputId = "d", label = "X max", value = 100000)
              ),
              mainPanel(plotOutput(outputId = "hist"))
            )
            ),
-  tabPanel("tab2", dataTableOutput(outputId = "data")),
-  tabPanel("tab3", "contents")
+  tabPanel("Boxplots", 
+           sidebarLayout(
+             sidebarPanel(
+               radioButtons(inputId = "e", label = "X var", choices = c(
+                 "Mechanic",
+                 "Domain"
+               )),
+               radioButtons(inputId = "f", label = "Y var", choices = c(
+                 "Year.Published", 
+                 "Min.Players",
+                 "Max.Players",
+                 "Play.Time",
+                 "Min.Age",
+                 "Users.Rated",
+                 "Rating.Average",
+                 "Complexity.Average",
+                 "Owned.Users"
+               )),
+               textInput(inputId = "g", label = "1st domain or mechanic to plot"),
+               textInput(inputId = "h", label = "2st domain or mechanic to plot"),
+               textInput(inputId = "i", label = "3st domain or mechanic to plot")
+             ),
+             mainPanel(
+               plotOutput(outputId = "boxplot")
+             )
+           )
+           )
 )
 )
 
@@ -40,7 +74,25 @@ server <- function(input, output, session) {
   
   
   output$hist <- renderPlot({
-    ggplot(game_data, aes_string(x = input$a)) + geom_histogram(binwidth = input$b)
+    ggplot(game_data, aes_string(x = input$a)) + geom_histogram(binwidth = input$b) + xlim(input$c, input$d)
+  })
+  
+  re <- reactive({
+    val <- input$e
+    if (val == "Mechanic") {
+      
+      return(ggplot(mechanic_data, aes(x = mechanic_data$Mechanics)) + geom_boxplot(aes_string(y = input$f))) 
+      
+    } else {
+      
+      return(ggplot(domain_data, aes(x = domain_data$Domains)) + geom_boxplot(aes_string(y = input$f)))
+    
+    }
+  })
+  
+  
+  output$boxplot <- renderPlot({
+    re()
   })
 }
 
